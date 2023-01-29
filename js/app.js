be9f8071628515ad47949653fe9c22229d506c09
Gastón <gastondimarco2013@gmail.com>
2023-01-29 07:46:56 -0300

    125 listo

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',function(){
+    //seleccionar elementos de la interfaz
+    const InputEmail = document.querySelector('#email')
+    const InputAsunto = document.querySelector('#asunto')
+    const InputMensaje = document.querySelector('#mensaje')
+
+    console.log(InputEmail)
+    console.log(InputAsunto)
+    console.log(InputMensaje)
+    
+})
