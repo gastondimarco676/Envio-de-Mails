ddf44a8fb1affe72ae59b81381f900213c4f7ac3
Gastón <gastondimarco2013@gmail.com>
2023-01-30 04:04:17 -0300

    131, clases de Talwind para alerta

--- js/app.js
+++ js/app.js
@@ -1,19 +1,26 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function () {
     //seleccionar elementos de la interfaz
     const InputEmail = document.querySelector('#email')
     const InputAsunto = document.querySelector('#asunto')
     const InputMensaje = document.querySelector('#mensaje')
 
-    InputEmail.addEventListener('blur',validar)
-    InputAsunto.addEventListener('blur',validar)
-    InputMensaje.addEventListener('blur',validar)
-    
-    function validar(e) {
-        e.target.value.trim()===''?console.log('nada che'):console.log('ahora si')
-    }
-     }
-     )
+    const formulario = document.querySelector('#formulario')
 
+    InputEmail.addEventListener('blur', validar)
+    InputAsunto.addEventListener('blur', validar)
+    InputMensaje.addEventListener('blur', validar)
+}
+)
+function validar(e) {
+    e.target.value.trim() === '' ? mostrarAlerta() : console.log('ahora si')
+}
+
+const mostrarAlerta = ()=>{
+    const alerta = document.createElement('p')
+    alerta.textContent ='no hay nada che, imprimiendo en html porque nadie navega con la consola abierta'
+    alerta.classList.add('bg-red-600','text-white','p-2')
+    formulario.appendChild(alerta)
+}
 
 
 
